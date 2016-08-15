484486fb300e4475e8ff4dca919ba651361e2b1c
sandeep <sandeepkumarpani888@gmail.com>
2016-08-15 17:58:22 +0530

    A problem with GCDS.

---
+++ CODEFORCES/475-D.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+
+//using namespace __gnu_pbds;
+using namespace std;
+
+#pragma comment(linker, "/STACK:16777216")
+
+#define Foreach(i, c) for(__typeof((c).begin()) i = (c).begin(); i != (c).end(); ++i)
+#define For(i,a,b) for(int (i)=(a);(i) < (b); ++(i))
+#define rof(i,a,b) for(int (i)=(a);(i) > (b); --(i))
+#define rep(i, c) for(auto &(i) : (c))
+#define x first
+#define y second
+#define pb push_back
+#define mp make_pair
+#define PB pop_back()
+#define iOS ios_base::sync_with_stdio(false)
+#define sqr(a) (((a) * (a)))
+#define all(a) a.begin() , a.end()
+#define error(x) cerr << #x << " = " << (x) <<endl
+#define Error(a,b) cerr<<"( "<<#a<<" , "<<#b<<" ) = ( "<<(a)<<" , "<<(b)<<" )\n";
+#define Error3(a,b,c) cerr<<"("<<#a<<" , "<<#b<<" , "<<#c<<" ) = ( "<<(a)<<" , "<<(b)<<" , "<<(c)<<")\n";
+#define Error4(a,b,c,d) cerr<<"("<<#a<<" , "<<#b<<" , "<<#c<<" , "<<#d<<" ) = ( "<<(a)<<" , "<<(b)<<" , "<<(c)<<" , "<<(d)<<")\n";
+#define errop(a) cerr<<#a<<" = ( "<<((a).x)<<" , "<<((a).y)<<" )\n";
+#define coud(a,b) cout<<fixed << setprecision((b)) << (a)
+#define L(x) (((x)<<1)+1)
+#define R(x) (((x)<<1)+2)
+#define umap unordered_map
+//#define double long double
+#define mod 1000000007
+// #define mod1 1000000009
+#define LIMIT 10000000000000000LL
+#define INF 10000000000LL
+#define MAX1 1000000000
+//#define si(n) scanf("%d",&n)
+//#define sii(n,m) scanf("%d%d",&n,&m)
+//#define pi(n) printf("%d\n",n)
+const int inf=0x3f3f3f3f;
+const long double pi=acos(-1.0);
+#define MAX 50010
+#define N 100010
+#define M 2000010
+const string debug_line="yolo";
+#define debug error(debug_line)
+const double PI=4*atan(1);
+#define read() freopen("mergedoutput.txt","r",stdin)
+#define write() freopen("output.txt","w",stdout)
+//template <typename T> using os =  tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+typedef long long ll;
+typedef pair<int,int>pii;
+typedef vector<int> vi;
+typedef complex<double> point;
+
+map<int,ll> mp1;
+map<int,ll> mp2;
+map<int,ll> mp3;
+int a[N];
+
+int main(){
+	int n;
+	scanf("%d",&n);
+
+	For(i,0,n){
+		int temp;
+		scanf("%d",&temp);
+		a[i]=temp;
+	}
+
+	For(i,0,n){
+		mp3.clear();
+		rep(t1,mp2){
+			mp3[__gcd(t1.x,a[i])]+=t1.y;
+		}
+		rep(t1,mp3){
+			mp1[t1.x]+=t1.y;
+		}
+		mp3[a[i]]++;
+		mp1[a[i]]++;
+		mp2=mp3;
+		// error(i);
+		// rep(t1,mp1){
+		// 	Error(t1.x,t1.y);
+		// }
+	}
+
+	int q;
+	scanf("%d",&q);
+	while(q--){
+		int temp;
+		scanf("%d",&temp);
+		printf("%lld\n",mp1[temp]);
+	}
+return 0;}
