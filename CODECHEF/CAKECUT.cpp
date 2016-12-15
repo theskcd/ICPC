1c948a704a36d4d5ddbf244ac2c1bf6497eb3f78
Sandeep Kumar Pani <sandeepkumarpani888@gmail.com>
2016-12-15 20:59:05 +0530

    CAKECUT

---
+++ CODECHEF/CAKECUT.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+
+//using namespace __gnu_pbds;
+using namespace std;
+
+// (づ°ω°)づﾐe★゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・
+
+#define Foreach(i, c) for(__typeof((c).begin()) i = (c).begin(); i != (c).end(); ++i)
+#define For(i,a,b) for(int (i)=(a);(i) < (b); ++(i))
+#define rof(i,a,b) for(int (i)=(a);(i) > (b); --(i))
+#define rep(i, c) for(auto &(i) : (c))
+#define x first
+#define y second
+#define pb push_back
+#define mp make_pair
+#define PB pop_back()
+#define iOS ios_base::sync_with_stdio(false)
+#define sqr(a) (((a) * (a)))
+#define all(a) a.begin() , a.end()
+#define error(x) cerr << #x << " = " << (x) <<endl
+#define Error(a,b) cerr<<"( "<<#a<<" , "<<#b<<" ) = ( "<<(a)<<" , "<<(b)<<" )\n";
+#define Error3(a,b,c) cerr<<"("<<#a<<" , "<<#b<<" , "<<#c<<" ) = ( "<<(a)<<" , "<<(b)<<" , "<<(c)<<")\n";
+#define Error4(a,b,c,d) cerr<<"("<<#a<<" , "<<#b<<" , "<<#c<<" , "<<#d<<" ) = ( "<<(a)<<" , "<<(b)<<" , "<<(c)<<" , "<<(d)<<")\n";
+#define Error5(a,b,c,d,e) cerr<<"("<<#a<<" , "<<#b<<" , "<<#c<<" , "<<#d<<" , "<<#e<<" ) = ( "<<(a)<<" , "<<(b)<<" , "<<(c)<<" , "<<(d)<<" , "<<(e)<<")\n";
+#define errop(a) cerr<<#a<<" = ( "<<((a).x)<<" , "<<((a).y)<<" )\n";
+#define coud(a,b) cout<<fixed << setprecision((b)) << (a)
+#define L(x) (((x)<<1)+1)
+#define R(x) (((x)<<1)+2)
+#define umap unordered_map
+//#define double long double
+#define mod 1000000007
+#define mod1 1000000009
+#define LIMIT 1000000000000000000LL
+#define MAX1 1000000000
+const int inf=0x3f3f3f3f;
+const long double pi=acos(-1.0);
+#define MAXN 150010
+const string debug_line="yolo";
+#define debug error(debug_line)
+#define read() freopen("mergedoutput.txt","r",stdin)
+#define write() freopen("output.txt","w",stdout)
+//template <typename T> using os =  tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+typedef long long ll;
+typedef pair<int,int>pii;
+typedef vector<int> vi;
+typedef complex <long double> complex_t;
+const long double PI = acos((long double)-1.0);
+
+char s[1510][1510];
+bitset<1510> bits[1510];
+int nums[1510][1510];
+int xor1[1510][1510];
+
+int main(){
+    int n,m;
+    scanf("%d%d",&n,&m);
+    For(i,0,n){
+        scanf("%s",s[i]);
+    }
+
+    For(i,0,n){
+        For(j,0,m){
+            nums[i+1][j+1]=s[i][j]-'0';
+        }
+    }
+
+    For(i,1,n+1){
+        For(j,1,m+1){
+            xor1[i][j]=nums[i][j]^xor1[i-1][j]^xor1[i][j-1]^xor1[i-1][j-1];
+            bits[i][j]=xor1[i][j];
+        }
+    }
+
+    ll answer1=0;
+
+    For(i,0,n){
+        For(j,i+1,n+1){
+            bitset<1510> res=bits[i] ^ bits[j];
+            ll count1=res.count();
+            ll count0=m+1-count1;
+            answer1+=(((count1-1)*count1)+((count0-1)*count0))/2;
+        }
+    }
+    printf("%lld\n",answer1);
+return 0;}
