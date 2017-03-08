c3932dbab211c1867f0fab864017e3a59dae5f25
Sandeep Kumar Pani <sandeepkumarpani888@gmail.com>
2017-03-08 23:01:24 +0530

    DFS

---
+++ CODEFORCES/781-C.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+
+//using namespace __gnu_pbds;
+using namespace std;
+
+// (づ°ω°)づﾐe★゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜
+
+#define Foreach(i, c) for(__typeof((c).begin()) i = (c).begin(); i != (c).end(); ++i)
+#define For(i,a,b) for(int (i)=(a);(i) < (b); ++(i))
+#define rof(i,a,b) for(int (i)=(a);(i) > (b); --(i))
+#define rep(i, c) for(auto &(i) : (c))
+#define x first
+#define y second
+#define pb push_back
+#define mp make_pair
+#define PB pop_back()
+#define iOS ios_base::sync_with_stdio(false)
+#define sqr(a) (((a) * (a)))
+#define all(a) a.begin() , a.end()
+#define error(x) cerr << #x << " = " << (x) <<endl
+#define Error(a,b) cerr<<"( "<<#a<<" , "<<#b<<" ) = ( "<<(a)<<" , "<<(b)<<" )\n";
+#define Error3(a,b,c) cerr<<"("<<#a<<" , "<<#b<<" , "<<#c<<" ) = ( "<<(a)<<" , "<<(b)<<" , "<<(c)<<")\n";
+#define Error4(a,b,c,d) cerr<<"("<<#a<<" , "<<#b<<" , "<<#c<<" , "<<#d<<" ) = ( "<<(a)<<" , "<<(b)<<" , "<<(c)<<" , "<<(d)<<")\n";
+#define Error5(a,b,c,d,e) cerr<<"("<<#a<<" , "<<#b<<" , "<<#c<<" , "<<#d<<" , "<<#e<<" ) = ( "<<(a)<<" , "<<(b)<<" , "<<(c)<<" , "<<(d)<<" , "<<(e)<<")\n";
+#define errop(a) cerr<<#a<<" = ( "<<((a).x)<<" , "<<((a).y)<<" )\n";
+#define coud(a,b) cout<<fixed << setprecision((b)) << (a)
+#define L(x) (((x)<<1)+1)
+#define R(x) (((x)<<1)+2)
+#define umap unordered_map
+//#define double long double
+#define mod 1000000007
+#define mod1 1000000009
+#define LIMIT 10000000000000000LL
+#define INF 10000000000LL
+#define MAX1 1000000000
+const int inf=0x3f3f3f3f;
+const long double pi=acos(-1.0);
+#define MAX 810
+// #define N 100010
+const string debug_line="yolo";
+#define debug error(debug_line)
+#define read() freopen("mergedoutput.txt","r",stdin)
+#define write() freopen("output.txt","w",stdout)
+//template <typename T> using os =  tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+typedef long long ll;
+typedef pair<int,int>pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef unsigned long long ull;
+typedef complex <long double> complex_t;
+const long double PI = acos((long double)-1.0);
+const long double eps=1e-6;
+ull getRand(){return ((ull)rand()<<40)+((ull)rand()<<20)+((ull)rand());}
+
+vi Adj1[200010];
+vi Adj[200010];
+bool visited[200010];
+vi order1;
+
+void DFS(int u,int p){
+	visited[u]=true;
+	order1.pb(u);
+	rep(t1,Adj1[u]){
+		if(t1!=p && !visited[t1]){
+			DFS(t1,u);
+			order1.pb(u);
+		}
+	}
+}
+
+int main(){
+	int n,m,k;
+	scanf("%d%d%d",&n,&m,&k);
+	For(i,0,m){
+		int x1,y1;
+		scanf("%d%d",&x1,&y1);
+		Adj1[x1].pb(y1);Adj1[y1].pb(x1);
+	}
+	DFS(1,0);
+	error(order1.size());
+	int limit1=(int)ceil((double)(2*n)/(double)k);
+	error(limit1);
+	int pointer1=0;
+	For(k1,0,k){
+		vi toPrint;
+		int size1=0;
+		while(pointer1<(int)order1.size() && size1<limit1){
+			toPrint.pb(order1[pointer1]);
+			pointer1++;
+			size1++;
+		}
+		if(toPrint.size()==0){
+			toPrint.pb(1);
+		}
+		printf("%d ",(int)toPrint.size());
+		rep(t1,toPrint){
+			printf("%d ",t1);
+		}
+		printf("\n");
+	}
+return 0;}
