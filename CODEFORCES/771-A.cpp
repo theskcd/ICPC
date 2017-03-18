c149d40c3c7a85374a213ef0a37062fe4da20f98
Sandeep Kumar Pani <sandeepkumarpani888@gmail.com>
2017-03-19 00:54:04 +0530

    DFS.

---
+++ CODEFORCES/771-A.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+
+// using namespace __gnu_pbds;
+using namespace std;
+
+// (づ°ω°)づﾐe★゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜
+
+#define Foreach(i, c) for(__typeof((c).begin()) i = (c).begin(); i != (c).end(); ++i)
+#define For(i,a,b) for(int (i)=(a);(i) < (b); ++(i))
+#define rof(i,a,b) for(int (i)=(a);(i) > (b); --(i))
+#define rep(i, c) for(auto &(i) : (c))
+#define x first
+#define y second
+#define pb push_back
+#define mp make_pair
+#define PB pop_back()
+#define iOS ios_base::sync_with_stdio(false)
+#define sqr(a) (((a) * (a)))
+#define all(a) a.begin() , a.end()
+#define error(x) cerr << #x << " = " << (x) <<endl
+#define Error(a,b) cerr<<"( "<<#a<<" , "<<#b<<" ) = ( "<<(a)<<" , "<<(b)<<" )\n";
+#define Error3(a,b,c) cerr<<"("<<#a<<" , "<<#b<<" , "<<#c<<" ) = ( "<<(a)<<" , "<<(b)<<" , "<<(c)<<")\n";
+#define Error4(a,b,c,d) cerr<<"("<<#a<<" , "<<#b<<" , "<<#c<<" , "<<#d<<" ) = ( "<<(a)<<" , "<<(b)<<" , "<<(c)<<" , "<<(d)<<")\n";
+#define Error5(a,b,c,d,e) cerr<<"("<<#a<<" , "<<#b<<" , "<<#c<<" , "<<#d<<" , "<<#e<<" ) = ( "<<(a)<<" , "<<(b)<<" , "<<(c)<<" , "<<(d)<<" , "<<(e)<<")\n";
+#define errop(a) cerr<<#a<<" = ( "<<((a).x)<<" , "<<((a).y)<<" )\n";
+#define coud(a,b) cout<<fixed << setprecision((b)) << (a)
+#define L(x) (((x)<<1)+1)
+#define R(x) (((x)<<1)+2)
+#define umap unordered_map
+//#define double long double
+#define mod 1000000007
+#define mod1 1000000009
+#define LIMIT 10000000000000000LL
+#define INF 10000000000LL
+#define MAX1 1000000000
+const int inf=0x3f3f3f3f;
+const long double pi=acos(-1.0);
+#define MAX 810
+// #define N 100010
+const string debug_line="yolo";
+#define debug error(debug_line)
+#define read() freopen("mergedoutput.txt","r",stdin)
+#define write() freopen("output.txt","w",stdout)
+typedef long long ll;
+typedef pair<int,int>pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef unsigned long long ull;
+typedef complex <long double> complex_t;
+const long double PI = acos((long double)-1.0);
+const long double eps=1e-6;
+// typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> os;
+ull getRand(){return ((ull)rand()<<40)+((ull)rand()<<20)+((ull)rand());}
+
+vi Adj[150010];
+bool poss=true;
+bool visited[150010];
+vi currNodes;
+map<pii,int> mp1;
+
+void DFS(int u){
+	visited[u]=true;
+	currNodes.pb(u);
+	rep(t1,Adj[u]){
+		if(!visited[t1]){
+			DFS(t1);
+		}
+	}
+}
+
+void checkConnected(){
+	rep(node,currNodes){
+		if(Adj[node].size()!=currNodes.size()-1){
+			poss=false;
+		}
+	}
+}
+
+int main(){
+	int n,m;
+	scanf("%d%d",&n,&m);
+	For(i,0,m){
+		int a1,b1;
+		scanf("%d%d",&a1,&b1);
+		Adj[a1].pb(b1);
+		Adj[b1].pb(a1);
+		mp1[mp(min(a1,b1),max(a1,b1))]=1;
+	}
+
+	For(i,1,n+1){
+		if(!visited[i]){
+			currNodes.clear();
+			DFS(i);
+			checkConnected();
+		}
+	}
+	if(poss){
+		printf("YES\n");
+	}
+	else{
+		printf("NO\n");
+	}
+return 0;}
